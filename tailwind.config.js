ea6f883cd5fc3aa1f92547557b3076d0198d7565
Coacoabro <99926234+Coacoabro@users.noreply.github.com>
2024-01-23 01:25:42 +0000

    css styling

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    colors: {
+      'kavita-gray': '#A0A0A0',
+    },
+  },
+  plugins: [],
+}
+
