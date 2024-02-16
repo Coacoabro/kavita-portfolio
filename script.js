618d3fb9115467ce288f159310d08968cd84a842
Coacoabro <aaLogan9@gmail.com>
2024-02-16 04:25:25 -0500

    Temporary Carousel commented

--- script.js
+++ script.js
@@ -153,3 +153,27 @@ images.forEach(function(image) {
     var text = image.nextElementSibling;
     text.textContent = image.alt;
 });
+
+/*
+var CarouselSlider = new Swiper('.carousel-slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+  */
